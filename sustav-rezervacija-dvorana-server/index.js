f17f5887c7d133fa4d5b8b5c06f09848bd2f0941
Crytonics <crytonics@gmail.com>
2024-04-24 21:40:14 +0200

    Dodani ostali upiti
    - Dodani ostali upiti
    - Ispravljene postavke za token

--- sustav-rezervacija-dvorana-server/index.js
+++ sustav-rezervacija-dvorana-server/index.js
@@ -45,10 +45,38 @@ app.get("/api/korisnici", authJwt.verifyTokenAdmin, (req, res) => {
 });
 
 // login
+app.post("/login", function (req, res) {
+  const data = req.body;
+  const korisnickoIme = data.korisnicko_ime;
+  const password = data.password;
+
+  connection.query("SELECT * FROM korisnici WHERE korisnicko_ime = ? AND lozinka = ?", [korisnickoIme, password], function (err, result) {
+    if (err) {
+      res.status(500).json({ success: false, message: "Internal server error" });
+    } else if (result.length > 0) {
+      res.status(200).json({ success: true, message: "Prijava uspješna!" });
+    } else {
+      res.status(401).json({ success: false, message: "Krivo korisničko ime ili lozinka!" });
+    }
+  });
+});
+
+//logout
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error("Error destroying session:", err);
+      res.status(500).json({ message: "Internal server error" });
+    } else {
+      res.status(200).json({ message: "Logout successful" });
+    }
+  });
+});
+
 // Unos novog korisnika (tablica "korisnici")
-app.post('/UnosKorisnikaAdministrator', function (request, response) {
+app.post('/unosKorisnika', authJwt.verifyTokenAdmin, function (request, response) {
   const data = request.body;
-  korisnik = [[data.id_korisnik, data.korisničko_ime,  data.ime, data.prezime, data.lozinka, data.uloga]]
+  korisnik = [[data.id_korisnik, data.korisnicko_ime,  data.ime, data.prezime, data.lozinka, data.uloga]]
   connection.query('INSERT INTO korisnci (id_korisnik, korisnicko_ime, ime, prezime, lozinka, uloga) VALUES ?',
   [korisnik], function (error, results, fields) {
     if (error) throw error;
@@ -56,8 +84,9 @@ app.post('/UnosKorisnikaAdministrator', function (request, response) {
     return response.send({ error: false, data: results, message: 'Korisnik je dodan.' });
   });
 });
+
 // Unos novog studijskog programa (tablica "studijskiProgrami")
-app.post('/UnosStudijskihProgramaAdministrator', function (request, response) {
+app.post('/unosStudijskogPrograma', authJwt.verifyTokenAdmin, function (request, response) {
   const data = request.body;
   studijskiprogram = [[data.id_StudijskogPrograma, data.naziv]]
   connection.query('INSERT INTO studijskiProgrami (id_StudijskogPrograma, naziv) VALUES ?',
@@ -67,8 +96,9 @@ app.post('/UnosStudijskihProgramaAdministrator', function (request, response) {
     return response.send({ error: false, data: results, message: 'Korisnik je dodan.' });
   });
 });
+
 // Unos novog kolegija (tablica "kolegij")
-app.post('/UnosStudijskihProgramaAdministrator', function (request, response) {
+app.post('/unosKolegija', authJwt.verifyTokenAdmin, function (request, response) {
   const data = request.body;
   novikolegij = [[data.id_kolegija, data.naziv,  data.id_korisnika, data.id_StudijskogPrograma]]
   connection.query('INSERT INTO korisnci (id_kolegija, naziv) VALUES ?',
@@ -78,8 +108,9 @@ app.post('/UnosStudijskihProgramaAdministrator', function (request, response) {
     return response.send({ error: false, data: results, message: 'Korisnik je dodan.' });
   });
 });
+
 // Unos nove dvorane (tablica "dvorane")
-app.post('/UnosDvoranaAdministrator', function (request, response) {
+app.post('/unosDvorane', authJwt.verifyTokenAdmin, function (request, response) {
   const data = request.body;
   dvorana = [[data.id_dvorane, data.naziv,  data.opis, data.svrha]]
   connection.query('INSERT INTO dvorane (id_dvorane, naziv, opis, svrha) VALUES ?',
@@ -89,6 +120,7 @@ app.post('/UnosDvoranaAdministrator', function (request, response) {
     return response.send({ error: false, data: results, message: 'Korisnik je dodan.' });
   });
 });
+
 // Prelged/Dohvati zauzeća dvorane (tablica "entry")
 app.get("/api/entry/:id", (req, res) => {
   const { id } = req.params;
@@ -102,9 +134,53 @@ app.get("/api/entry/:id", (req, res) => {
       }
   );
 });
-// Pregled/Dohvati popis nastavnika (tablica "korisnici") (uloga = nastavnik)
+
+// Zahtjev rezervacija dvorana
+app.post('/api/zahtjevRezervacija', authJwt.verifyTokenAdminOrUser, function (request, response) {
+  const data = request.body;
+  zahtjev = [[data.id_entry, data.naziv,  data.tip, data.opis, data.start_time, data.end_time, data.id_dvorane, data.id_korisnik, data.id_kolegij, data.id_studijskiProgrami]]
+  connection.query('INSERT INTO entry (id_entry, naziv, tip, opis, start_time, end_time, id_dvorane, id_korisnik, id_kolegij, id_studijskiProgrami) VALUES ?', [zahtjev], function (error, results, fields) {
+    if (error) throw error;
+    console.log('data', data)
+    return response.send({ error: false, data: results, message: 'Korisnik je dodan.' });
+  });
+});
+
+// Pregled rezervacija
+app.get('/api/rezervacije', authJwt.verifyTokenAdminOrUser, (req, res) => {
+  connection.query("SELECT * FROM entry", (error, results) => {
+    if (error) throw error;
+
+    res.send(results);
+  });
+});
+
+// Pregled/Dohvati popis korisnika
+app.get('/api/korisnici', authJwt.verifyTokenAdmin, (req, res) => {
+  connection.query("SELECT * FROM korisnici", (error, results) => {
+    if (error) throw error;
+
+    res.send(results);
+  });
+});
+
 // Pregled/Dohvati dvorane (tablica "dvorane")
+app.get('/api/dvorane', (req, res) => {
+  connection.query("SELECT * FROM dvorane", (error, results) => {
+    if (error) throw error;
+
+    res.send(results);
+  });
+});
+
 // Pregled/Dohvati kolegiji (tablica "kolegij")
+app.get('/api/kolegiji', (req, res) => {
+  connection.query("SELECT * FROM kolegij", (error, results) => {
+    if (error) throw error;
+
+    res.send(results);
+  });
+});
 
 app.listen(port, () => {
   console.log("Server running at port: " + port);
