1ded370aed8f6d4ed1d23ac7ffbe41c8a20b977d
Crytonics <crytonics@gmail.com>
2024-04-30 13:26:04 +0200

    Prikaz studenskih programa (Administrator)
    - Sada se iz baze prikažu podaci u tablicu
    - Gumbovi rade

--- sustav-rezervacija-dvorana/src/pages/Administrator/StudijskiProgramiAdministrator.jsx
+++ sustav-rezervacija-dvorana/src/pages/Administrator/StudijskiProgramiAdministrator.jsx
@@ -1,22 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 export default  function StudijskiProgramiAdministrator() {
 
     const [searchTerm, setSearchTerm] = useState('');
 
-    const [programs, setPrograms] = useState([
-        { id: 1, naziv: 'Informatika' },
-        { id: 2, naziv: 'Telematika' },
-        { id: 3, naziv: 'Promet' },
+    const [studenskiProgrami, setStudenskiProgrami] = useState([]);
+
+    const [stupci, setStupci] = useState([
+        {
+            name: "naziv",
+            required: true,
+            label: "Naziv",
+            align: "left",
+            field: "naziv",
+            sortable: true,
+        },
+        {
+            name: "funkcije",
+            label: "Funkcije",
+            align: "center",
+        }
     ]);
 
     const handleSearchChange = (event) => {
         setSearchTerm(event.target.value);
     };
 
-    const filteredPrograms = programs.filter(program =>
-        program.naziv.toLowerCase().includes(searchTerm.toLowerCase())
+    const filteredStudenskiProgrami = studenskiProgrami.filter(studprog =>
+        studprog.naziv.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const filteredStupci = stupci.filter(stupc =>
+        stupc.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     const navigate = useNavigate();
@@ -25,12 +42,46 @@ export default  function StudijskiProgramiAdministrator() {
         navigate("/unosStudijskihProgramaAdministrator");
     }
 
-    const vidi_stisnuto = () => {
-        navigate("/pojediniKolegijiPoStudijimaSvi");
+    const vidi_stisnuto = (idStudProg) => {
+        navigate(`/pojediniKolegijiPoStudijimaSvi/${idStudProg}`);
     }
     
-    const uredi_stisnuto = () => {
-        navigate("/azuriranjeStudijskihProgramaAdministrator");
+    const uredi_stisnuto = async (idStudProg) => {
+        navigate(`/azuriranjeStudijskihProgramaAdministrator/${idStudProg}`);
+    }
+
+    useEffect(() => {
+        async function fetchInitialData() {
+            // Get the JWT token from local storage
+            const token = localStorage.getItem("token");
+        
+            // Set up the request headers to include the JWT token
+            const headers = { Authorization: `Bearer ${token}` };
+
+            dohvatiStudijskePrograme(headers);
+        }
+
+        fetchInitialData();
+    }, []);
+
+    const dohvatiStudijskePrograme = async (headers) => {
+        try {
+            const response = await axios.get("http://localhost:3000/api/studijskiProgrami", {headers});
+            setStudenskiProgrami(response.data);
+        } catch (error) {
+            console.log("Greška prilikom dohvata studijskih programa:", error);
+        }
+    }
+
+    const obrisiStudijskiProgram = async (idStudProg) => {
+        const token = localStorage.getItem("token");
+        const headers = { Authorization: `Bearer ${token}` };
+        if(window.confirm('Jeste li sigurni da želite obrisati studijski program?')) {
+            const response = await axios.put("http://localhost:3000/api/onemoguciStudijskiProgram/" + idStudProg, null, {headers}); //null zbog PUT 'payloada'
+            const response2 = await axios.get("http://localhost:3000/api/studijskiProgrami", {headers});
+            setStudenskiProgrami(response2.data);
+            window.alert('Studijski program je uspješno izbrisan.')
+        }
     }
 
     return (
@@ -54,18 +105,21 @@ export default  function StudijskiProgramiAdministrator() {
             <table>
                 <thead>
                     <tr>
-                        <th>Naziv</th>
-                        <th>Funkcije</th>
+                        {filteredStupci.length > 0 && 
+                        filteredStupci.map(stupc => (
+                            <th key={stupc.label}>{stupc.label}</th>
+                        ))}
                     </tr>
                 </thead>
                 <tbody>
-                    {filteredPrograms.map((program) => (
-                        <tr key={program.id}>
-                            <td>{program.naziv}</td>
+                    {filteredStudenskiProgrami.map((studprog, index) => (
+                        <tr key={index}>
+                            <td>{studprog.naziv}</td>
+                            
                             <td>
-                                <button className="gumb_vidi" onClick={vidi_stisnuto}>Vidi</button>
-                                <button className="gumb_uredi" onClick={uredi_stisnuto}>Uredi</button>
-                                <button className="gumb_obriši">Izbriši</button>
+                                <button className="gumb_vidi" onClick={() => vidi_stisnuto(studprog.id_studijskogPrograma)}>Vidi</button>
+                                <button className="gumb_uredi" onClick={() => uredi_stisnuto(studprog.id_studijskogPrograma)}>Uredi</button>
+                                <button className="gumb_obriši" onClick={() => obrisiStudijskiProgram(studprog.id_studijskogPrograma)}>Izbriši</button>
                             </td>
                         </tr>
                     ))}
