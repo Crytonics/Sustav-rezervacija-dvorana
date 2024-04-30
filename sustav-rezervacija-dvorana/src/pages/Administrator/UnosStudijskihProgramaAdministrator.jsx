1e626afa12e7ff1226bb242d46b796a357986f20
Crytonics <crytonics@gmail.com>
2024-04-30 13:26:32 +0200

    Unos studenskih programa (Administrator))
    - Omogućeno unošenje studentskih programa u bazu

--- sustav-rezervacija-dvorana/src/pages/Administrator/UnosStudijskihProgramaAdministrator.jsx
+++ sustav-rezervacija-dvorana/src/pages/Administrator/UnosStudijskihProgramaAdministrator.jsx
@@ -1,4 +1,5 @@
 import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 export default function UnosStudijskihProgramaAdministrator() {
 
@@ -8,9 +9,30 @@ export default function UnosStudijskihProgramaAdministrator() {
         navigate("/kolegijiAdministrator");
     }
 
+    const spremi_podatke = (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+        const naziv_studija = event.target.naziv.value;
+
+        const userData = { naziv_studija };
+
+        posalji_podatke(userData);
+    }
+
+    const posalji_podatke = async (userData) => {
+        const token = localStorage.getItem("token");
+        const headers = { Authorization: `Bearer ${token}` };
+        try {
+            const response = await axios.post("http://localhost:3000/api/unosStudijskogPrograma", userData, {headers});
+        } catch (error) {
+            console.log("Greška prilikom dohvata korisnika:", error);
+        }
+        window.alert('Korisnik uspješno dodan.')
+        navigate("/studijskiProgramiAdministrator");
+    }
+
     return (
         <div className="unos-korisnika-container">
-        <form className="login-form">
+        <form className="login-form" onSubmit={spremi_podatke}>
             <div className="form-group">
                 <label htmlFor="naziv">Naziv studija: </label>
                 <input type="text" id="naziv" name="naziv" required />
