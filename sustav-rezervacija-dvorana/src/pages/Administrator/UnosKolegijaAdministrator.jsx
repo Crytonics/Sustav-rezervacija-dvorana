f861e456a77282a754dbd041b435f024d780ad29
Crytonics <crytonics@gmail.com>
2024-04-30 13:32:23 +0200

    Unos kolegija (Administrator)
    - Omogućeno unošenje kolegijau bazu

--- sustav-rezervacija-dvorana/src/pages/Administrator/UnosKolegijaAdministrator.jsx
+++ sustav-rezervacija-dvorana/src/pages/Administrator/UnosKolegijaAdministrator.jsx
@@ -1,6 +1,48 @@
+import React, { useState, useEffect } from 'react';
 import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 export default function UnosKolegijaAdministrator() {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const [korisnici, setKorisnici] = useState([]);
+
+    const [studenskiProgrami, setStudenskiProgrami] = useState([]);
+
+    useEffect(() => {
+        async function fetchInitialData() {
+
+            // Get the JWT token from local storage
+            const token = localStorage.getItem("token");
+        
+            // Set up the request headers to include the JWT token
+            const headers = { Authorization: `Bearer ${token}` }; 
+
+            try {
+                const response = await axios.get(`http://localhost:3000/api/korisnici`, { headers });
+                setKorisnici(response.data);
+
+                const response1 = await axios.get(`http://localhost:3000/api/studijskiProgrami`, { headers });
+                setStudenskiProgrami(response1.data);
+              
+            } catch (error) {
+                console.log("Greška prilikom dohvata podataka:", error);
+            } 
+            
+        }
+
+        fetchInitialData();
+    }, []);
+
+    const filteredKorisnici = korisnici.filter(korisnik =>
+        korisnik.ime.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        korisnik.prezime.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const filteredStudenskiProgrami = studenskiProgrami.filter(studprog =>
+        studprog.naziv.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     const navigate = useNavigate();
 
@@ -8,29 +50,56 @@ export default function UnosKolegijaAdministrator() {
         navigate("/kolegijiAdministrator");
     }
 
+    const spremi_podatke = (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+        const naziv_kolegija = event.target.naziv_kolegija.value;
+        const nastavnik = event.target.nastavnik.value;
+        const studijski_program = event.target.studijski_program.value;
+
+        const userData = { naziv_kolegija, nastavnik, studijski_program };
+
+        posalji_podatke(userData);
+    }
+
+    const posalji_podatke = async (userData) => {
+        const token = localStorage.getItem("token");
+        const headers = { Authorization: `Bearer ${token}` };
+        try {
+            const response = await axios.post("http://localhost:3000/api/unosKolegija", userData, {headers});
+        } catch (error) {
+            console.log("Greška prilikom spremanja kolegija:", error);
+        }
+        window.alert('Kolegij uspješno dodan.')
+        navigate("/kolegijiAdministrator");
+    }
+
     return (
         <div className="unos-korisnika-container">
-        <form className="login-form">
+        <form className="login-form" onSubmit={spremi_podatke}>
             <div className="form-group">
-                <label htmlFor="naziv">Naziv kolegija: </label>
-                <input type="text" id="naziv" name="naziv" required />
+                <label htmlFor="naziv_kolegija">Naziv kolegija: </label>
+                <input type="text" id="naziv_kolegija" name="naziv_kolegija" required />
             </div>
             <div className="form-group">
-                <label htmlFor="Nastavnik">Nastavnik: </label>
-                <select id="Nastavnik" name="Nastavnik" required>
+                <label htmlFor="nastavnik">Nastavnik: </label>
+                <select id="nastavnik" name="nastavnik" required>
                     <option value="">Odaberite nastavnika</option>
-                    <option value="nastavnik1">Primjer Izvođača</option>
-                    <option value="nastavnik2">Primjer Izvođača 2</option>
-                    <option value="nastavnik3">Primjer Izvođača 3</option>
+                    {filteredKorisnici.map((korisnik) => (
+                        <option key={korisnik.id_korisnik} value={korisnik.id_korisnik}>
+                            {korisnik.ime} {korisnik.prezime}
+                        </option>
+                    ))}
                 </select>
             </div>
             <div className="form-group">
-                <label htmlFor="Nastavnik">Studijski program: </label>
-                <select id="Studij" name="Studij" required>
+                <label htmlFor="studijski_program">Studijski program: </label>
+                <select id="studijski_program" name="studijski_program" required>
                     <option value="">Odaberite studijski program</option>
-                    <option value="Studij1">Informatika</option>
-                    <option value="Studij2">Telematika</option>
-                    <option value="Studij3">Promet</option>
+                    {filteredStudenskiProgrami.map((program) => (
+                        <option key={program.id_studijskogPrograma} value={program.id_studijskogPrograma}>
+                            {program.naziv}
+                        </option>
+                    ))}
                 </select>
             </div>
             <button type="button" onClick={natrak_stisnuto}>Natrag</button>
