b1c47c94b074eea19b31420a064de60fdfb2d855
Plakotic <plakotic@veleri.hr>
2024-05-02 11:45:02 +0200

    Admin_Dvorane
    Admin- Dvorane

--- sustav-rezervacija-dvorana/src/pages/Administrator/AzuriranjeDvoranaAdministrator.jsx
+++ sustav-rezervacija-dvorana/src/pages/Administrator/AzuriranjeDvoranaAdministrator.jsx
@@ -1,24 +1,87 @@
-import React, { useState } from 'react';
-import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from "react-router-dom";
+import axios from 'axios';
 
-export default function DvoraneAdministrator() {
+export default function AzuriranjeDvorane() {
 
     const navigate = useNavigate();
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const [dvorane, setDvorane] = useState([]);
 
     const natrak_stisnuto = () => {
         navigate("/dvoraneAdministrator");
     }
 
+    const { idDvorane } = useParams()
+
+    const filteredDvorane = dvorane.filter(dvorane =>
+        dvorane.naziv.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        dvorane.opis.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        dvorane.svrha.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        dvorane.aktivan.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+
+    useEffect(() => {
+        async function fetchInitialData() {
+            const token = localStorage.getItem("token");
+            const headers = { Authorization: `Bearer ${token}` }; 
+
+            try {
+                const response = await axios.get(`http://localhost:3000/api/dvorane/${idDvorane}`, { headers });
+                setDvorane(response.data);
+            } catch (error) {
+                console.log("Greška prilikom dohvata podataka:", error);
+            } 
+        }
+
+        fetchInitialData();
+    }, []);
+
+    const spremi_podatke = (event) => {
+        event.preventDefault();
+        const naziv = event.target.naziv.value;
+        const opis = event.target.opis.value;
+        const svrha = event.target.svrha.value;
+        const aktivan = event.target.aktivan.checked;
+
+        const userData = { naziv, opis, svrha, aktivan };
+
+        posalji_podatke(userData);
+    }
+
+    const posalji_podatke = async (userData) => {
+        const token = localStorage.getItem("token");
+        const headers = { Authorization: `Bearer ${token}` };
+        try {
+            const response = await axios.put(`http://localhost:3000/api/dvorane/${idDvorane}`, userData, {headers});
+        } catch (error) {
+            console.log("Greška prilikom ažuriranja dvorane:", error);
+        }
+        window.alert("Dvorana uspješno ažurirana.")
+        navigate("/dvoraneAdministrator");
+    }
+
     return (
-        <div className="unos-korisnika-container">
-        <form className="login-form">
+        <div className="unos-dvorane-container">
+        <form className="login-form" onSubmit={spremi_podatke}>
             <div className="form-group">
                 <label htmlFor="naziv">Naziv dvorane: </label>
-                <input type="text" id="naziv" name="naziv" placeholder='Dvorana 1' required />
+                <input type="text" id="naziv" name="naziv" placeholder={dvorane.naziv || 'Unesite naziv'} required />
             </div>
             <div className="form-group">
-                <label htmlFor="opis">Opis dvorane: </label>
-                <input type="text" id="opis" name="opis" placeholder='Računalna dvorana' required />
+                <label htmlFor="opis">Opis: </label>
+                <input type="text" id="opis" name="opis" placeholder={dvorane.opis || 'Unesite opis'} required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="svrha">Svrha: </label>
+                <input type="text" id="svrha" name="svrha" placeholder={dvorane.svrha || 'Unesite svrhu'} required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="aktivan">Aktivan: </label>
+                <input type="checkbox" id="aktivan" name="aktivan" defaultChecked={dvorane.aktivan} />
             </div>
             <button type="button" onClick={natrak_stisnuto}>Natrag</button>
             <button type="submit">Spremi</button>
--- sustav-rezervacija-dvorana/src/pages/Administrator/UnosDvoranaAdministrator.jsx
+++ sustav-rezervacija-dvorana/src/pages/Administrator/UnosDvoranaAdministrator.jsx
@@ -1,4 +1,6 @@
+import React, { useState, useEffect } from 'react';
 import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 export default function UnosDvoranaAdministrator() {
 
@@ -8,7 +10,60 @@ export default function UnosDvoranaAdministrator() {
         navigate("/dvoraneAdministrator");
     }
 
+    const spremi_podatke = (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+        const naziv = event.target.naziv.value;
+        const opis = event.target.opis.value;
+        const svrha = event.target.svrha.value;
+        const aktivan = event.target.aktivan.value;
+        
+
+        const userData = { naziv, opis, svrha, aktivan };
+
+        posalji_podatke(userData);
+    }
+
+    const posalji_podatke = async (userData) => {
+        const token = localStorage.getItem("token");
+        const headers = { Authorization: `Bearer ${token}` };
+        try {
+            const response = await axios.post("http://localhost:3000/api/unosDvorane", userData, {headers});
+        } catch (error) {
+            console.log("Greška prilikom dohvata dvorana:", error);
+        }
+        window.alert('Dvorana je  uspješno dodana.')
+        navigate("/listaDvoranaAdministrator");
+    }
+
     return (
+        <div className="unos-korisnika-container">
+        <form className="login-form" onSubmit={spremi_podatke}>
+            <div className="form-group">
+                <label htmlFor="naziv">Ime: </label>
+                <input type="text" id="naziv" name="naziv" required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="Opis">Opis: </label>
+                <input type="text" id="opis" name="opis" required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="svrha">Svrha: </label>
+                <input type="text" id="svrha" name="svrha" required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="aktivan">Aktivna: </label>
+                <input type="text" id="aktivan" name="aktivan" required />
+            </div>
+            
+            <button type="button" onClick={natrak_stisnuto}>Natrag</button>
+            <button type="submit">Spremi</button>
+        </form>
+    </div>
+    )
+}
+
+
+   /* return (
         <div className="unos-korisnika-container">
         <form className="login-form">
             <div className="form-group">
@@ -24,4 +79,4 @@ export default function UnosDvoranaAdministrator() {
         </form>
     </div>
     )
-}
+} */
