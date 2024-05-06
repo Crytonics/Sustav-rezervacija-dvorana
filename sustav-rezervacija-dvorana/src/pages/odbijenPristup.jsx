d2e614cf0ac5b7735f0ebc4c9a88c6a68feeb971
Plakotic <plakotic@veleri.hr>
2024-05-06 12:51:34 +0200

    Dodane stranice za 404 grešku i za pristup

---
+++ sustav-rezervacija-dvorana/src/pages/odbijenPristup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function odbijenPristup() {
+  return (
+    <div>
+      <h1>Oops. Nemate pristup ovoj stranici...</h1>
+    </div>
+  );
+}
+
+export default odbijenPristup;
